fba506192e90577ac10f5b51f0cd0f2494459ffd
Rushwanth-K <Rushwanth93@gmail.com>
2026-06-03 09:33:52 +0530

    Add port support for Railway MySQL

--- database.js
+++ database.js
@@ -9,6 +9,7 @@ dotenv.config();
 // A pool manages multiple connections efficiently
 const pool = mysql.createPool({
   host:     process.env.DB_HOST,
+  port:    process.env.DB_PORT || 3306,
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
