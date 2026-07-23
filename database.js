838540694d560fb570f6826a41f40779461c629e
Rushwanth-K <Rushwanth93@gmail.com>
2026-07-23 10:31:15 +0530

    fix: migrate database from Railway to Aiven

--- database.js
+++ database.js
@@ -9,10 +9,13 @@ dotenv.config();
 // A pool manages multiple connections efficiently
 const pool = mysql.createPool({
   host:     process.env.DB_HOST,
-  port:    process.env.DB_PORT || 3306,
+  port:     process.env.DB_PORT || 3306,
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  },
   waitForConnections: true,
   connectionLimit: 10,
 });
